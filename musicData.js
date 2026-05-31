308cccf8f29af3601b9afaf50bf29ca431f39903
shirasaki <278679647+shirasaki243@users.noreply.github.com>
2026-05-31 22:21:56 +0900

    Update musicData.js

--- musicData.js
+++ musicData.js
@@ -314,10 +314,15 @@ const musicData = [
     { name: "年越しあんさんぶる！！2022→2023", attr: "All", time: 214 },
     
 
-    // 2026.5.30時点、以下今後の拡張用　！「新しい曲を追加するときは、必ずファイルの『一番下（最後）』に書き足す」！ sparkle Brilliant Glitter Flash All　今後の拡張用
+    // 2026.5.31時点、以下今後の拡張用　！「新しい曲を追加するときは、必ずファイルの『一番下（最後）』に書き足す」！ sparkle Brilliant Glitter Flash All　今後の拡張用
     { name: "CALL YOUR NAME!!", attr: "All", time: 162 },
     { name: "CIVIL WAR", attr: "Flash", time: 154 },
     { name: "RED GARDEN", attr: "Glitter", time: 169 },
+    { name: "ソロ（こはく・晃牙・忍・弓弦）", attr: "Brilliant", time: 167 },
+    { name: "Unbalanced Triangle", attr: "Glitter", time: 154 },
+    { name: "最新", attr: "glitter", time: 120 },
+    { name: "最新", attr: "glitter", time: 120 },
+    { name: "最新", attr: "glitter", time: 120 },
     { name: "最新", attr: "glitter", time: 120 },
     { name: "最新", attr: "glitter", time: 120 },
     { name: "最新", attr: "glitter", time: 120 },
